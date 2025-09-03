3466d4ef7816693b31fb33cfd1de8b46575b8543
matbenv <mpbenvenutti@gmail.com>
2025-09-03 18:54:54 -0300

    antes da aula 03/09/25, finalizando exercicios de subprograma

--- exercicios/modularizacao/matriz/04/main.cpp
+++ exercicios/modularizacao/matriz/04/main.cpp
@@ -13,46 +13,85 @@ unsigned iinput(string, bool=true);
 void readMatrix_integer(unsigned, unsigned, int[][MAX]);
 void printMatrix_integer(unsigned, unsigned, int[][MAX]);
 void readMatrixSize(unsigned &, unsigned &);
-void matrixMultiplication_integer(unsigned, unsigned, int[][MAX], int[][MAX], int[][MAX]);
+void matrixMultiplication_integer(unsigned, unsigned, unsigned, int[][MAX], int[][MAX], int[][MAX]);
+unsigned matBColumns();
 
-//TODO
+int main(){
+   unsigned n, m, p;
+   int matA[MAX][MAX], matB[MAX][MAX], matC[MAX][MAX]{0};
+
+   cout << "Matrix A: \n";
+   readMatrixSize(n, m);
+
+   readMatrix_integer(n, m, matA);
+
+   p = matBColumns();
+   readMatrix_integer(m, p, matB);
+
+   matrixMultiplication_integer(n, m, p, matA, matB, matC);
+
+   cout << "Matrix A: \n";
+   printMatrix_integer(n, m, matA);
+   cout << "Matrix B: \n";
+   printMatrix_integer(m, p, matB);
+   cout << "Product Matrix: \n";
+   printMatrix_integer(n, p, matC);
+
+   return 0;
+}
+
 void matrixMultiplication_integer(unsigned n, unsigned m, unsigned p, int matA[][MAX], int matB[][MAX], int matC[][MAX]){
-    for(unsigned i = 0; i < n; i++){
+   for(unsigned i = 0; i < n; i++){
+      for(unsigned j = 0; j < p; j++){
+         for(unsigned k = 0; k < m; k++){
+            matC[i][j] += matA[i][k]*matB[k][j];
+         }
+      }
+   }
+}
 
-    }
+unsigned matBColumns(){
+   unsigned p;
+   cout << "Matrix B has to have the same amount of lines as A has of columns so they can be multipliable.\n"
+   << "Therefore, B line amount is set. Input how many columns B has: \n";
+   do{
+      p = iinput("", false);
+      if(p < MIN or p > MAX) cout << "Value out of range.\n";
+   }while(p < MIN or p > MAX);
+   return p;
 }
 
 void readMatrixSize(unsigned &n, unsigned &m){
    do{
-      n = iinput("How many lines: ", true);
+      n = iinput("How many lines: ", false);
       if(n < MIN or n > MAX) cout << "Value out of accepted range. Try between " << MIN << " and " << MAX << ". \n";
    }while(n < MIN or n > MAX);
    do{
-      m = iinput("How many columns: ", true);
+      m = iinput("How many columns: ", false);
       if(m < MIN or m > MAX) cout << "Value out of accepted range. Try between " << MIN << " and " << MAX << ". \n";
    }while(m < MIN or m > MAX);
 }
 
-void printMatrix_integer(unsigned m, unsigned n, int mat[][MAX]){
-   for(unsigned i = 0; i < m; i++){
+void printMatrix_integer(unsigned n, unsigned m, int mat[][MAX]){
+   for(unsigned i = 0; i < n; i++){
       cout << "|";
-      for(unsigned j = 0; j < n; j++){
+      for(unsigned j = 0; j < m; j++){
          cout << " " << mat[i][j];
       }
-      cout << " |\n";
+      cout << " |\t\n";
    }
 }
 
-void readMatrix_integer(unsigned m, unsigned n, int mat[][MAX]){
-   for(unsigned i = 0; i < m; i++){
-      for(unsigned j = 0; j < n; j++){
+void readMatrix_integer(unsigned n, unsigned m, int mat[][MAX]){
+   for(unsigned i = 0; i < n; i++){
+      for(unsigned j = 0; j < m; j++){
          cout << "Input element " << i+1 << "," << j+1 << ": ";
          mat[i][j] = iinput("", true);
       }
    }
 }
 
-bool isInt(string s, bool acceptNegative=true){
+bool isInt(string s, bool acceptNegative){
    if(acceptNegative){
       if(not isdigit(s[0]) and s[0] != '-') return false;
    }
@@ -62,7 +101,7 @@ bool isInt(string s, bool acceptNegative=true){
    return true;
 }
 
-unsigned iinput(string msg, bool acceptNegative=true){
+unsigned iinput(string msg, bool acceptNegative){
    string s;
    do{
       cout << msg; getline(cin, s);
